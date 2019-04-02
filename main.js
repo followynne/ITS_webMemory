233617fa0bc2918543c407d94dc4c1ca895edcc4
Matteo Gregoricchio <m.gregoricchio@studenti.local>
2019-04-02 10:13:52 +0200

    allineamento2

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-import $ from 'jquery'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import $ from 'jquery'
 
 $(document).ready(() => {
     console.log("yo");
